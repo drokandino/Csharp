64bfaeb3a2ccc45f802da18cd089b898b0c5b36a
Dino Drokan <dino.ddrokan@gmail.com>
2019-04-11 11:52:52 +0200

    Dodana klasa Cilindar koja nasljeduje kruznicu

---
+++ Geometrija/Geometrija/Cilindar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrija
+{
+    class Cilindar : Kruznica
+    {
+        private double visina = 1;
+
+        public Cilindar(int radius, int visina)
+        {
+            this.radius = radius;
+            this.visina = visina;
+        }
+        
+        public double volume() { return this.radius * 3.14 * this.visina; }
+    }
+}
