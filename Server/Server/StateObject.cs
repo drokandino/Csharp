daae5fbd300268d6f963484d9f115032262fff15
Dino Drokan <dino.ddrokan@gmail.com>
2019-06-03 20:35:13 +0200

    First commit

---
+++ Server/Server/StateObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server
+{
+    public class StateObject
+    {
+        public Socket workSocket = null; //client socket
+        public const int bufferSize = 1024;
+        public byte[] buffer = new byte[bufferSize];
+        public StringBuilder stringBuilder = new StringBuilder();
+    }
+}
